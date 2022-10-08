48a716b64c049567842d55da9401a5cc49d32c3e
Sherlock898 <aguayo.sebastian.r@gmail.com>
2022-10-08 13:45:04 -0300

    Tal vez sea así?

---
+++ tarea1/src/tarea1/Cliente.java
@@ -0,0 +1,15 @@
+package tarea1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author fravidal2021
+ */
+public class Cliente {
+
+    private String nombre;
+    private String rut;
+}
